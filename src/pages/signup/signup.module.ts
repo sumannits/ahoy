50164bb6a6eef32fc7ff2f24908990665a6101d4
sumannits <suman@natitsolved.com>
2018-06-11 18:51:04 +0530

    user add interest and search interest

--- src/pages/signup/signup.module.ts
+++ src/pages/signup/signup.module.ts
@@ -2,19 +2,25 @@ import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SignupPage } from './signup';
-//import { CommonModule } from '@angular/common';
+import { Ng4GeoautocompleteModule } from 'ng4-geoautocomplete';
+//import { Geolocation } from '@ionic-native/geolocation';
+import { NativeGeocoder } from '@ionic-native/native-geocoder';
 
 @NgModule({
   declarations: [
     SignupPage,
   ],
   imports: [
-    //CommonModule,
+    Ng4GeoautocompleteModule.forRoot(),
     IonicPageModule.forChild(SignupPage),
     TranslateModule.forChild()
   ],
   exports: [
     SignupPage
+  ],
+  providers: [
+    NativeGeocoder
+    //Geolocation
   ]
 })
 export class SignupPageModule { }
